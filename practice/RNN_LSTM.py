97c0c80c302d48c9194eb9d540e5b30729b78071
shizhonghui <>
2023-01-05 20:03:38 +0800

    模型参数

--- practice/RNN_LSTM.py
+++ practice/RNN_LSTM.py
@@ -12,55 +12,57 @@ hidden_size = 4
 num_layer = 1
 seq_len = 4
 
-idx_char = ['e','h','l','o']
-data_x = [1,0,2,2,3]
-data_y = [3,1,2,3,2]
+idx_char = ['e', 'h', 'l', 'o']
+data_x = [1, 0, 2, 2, 3]
+data_y = [3, 1, 2, 3, 2]
 
-one_hot_lookup = [[1,0,0,0],
-                  [0,1,0,0],
-                  [0,0,1,0],
-                  [0,0,0,1]
+one_hot_lookup = [[1, 0, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]
                   ]
 
 x_one_hot = [one_hot_lookup[i] for i in data_x]
-inputs = torch.Tensor(x_one_hot).view(-1,batch_size,input_size)
+inputs = torch.Tensor(x_one_hot).view(-1, batch_size, input_size)
 labels = torch.LongTensor(data_y).view(-1)
-print("input.shape,input,",(inputs.shape,inputs))
+print("input.shape,input,", (inputs.shape, inputs))
+
+
 class Model(nn.Module):
-    def __init__(self,batch_size,input_size,hidden_size,num_layer):
+    def __init__(self, batch_size, input_size, hidden_size, num_layer):
         super(Model, self).__init__()
         self.batch_size = batch_size
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layer = num_layer
 
-        self.rnn = nn.GRU(input_size=self.input_size,hidden_size=self.hidden_size,num_layers=self.num_layer)
+        self.rnn = nn.GRU(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layer)
 
-    def forward(self,inputs):
-        hx = torch.zeros(self.num_layer,self.batch_size,self.hidden_size)
-        #cx = torch.zeros(self.num_layer,self.batch_size,self.hidden_size)
-        out,_  = self.rnn(inputs,hx)
-        return out.view(-1,self.hidden_size)
+    def forward(self, inputs):
+        hx = torch.zeros(self.num_layer, self.batch_size, self.hidden_size)
+        # cx = torch.zeros(self.num_layer,self.batch_size,self.hidden_size)
+        out, _ = self.rnn(inputs, hx)
+        return out.view(-1, self.hidden_size)
 
-net = Model(batch_size,input_size,hidden_size,num_layer)
+
+net = Model(batch_size, input_size, hidden_size, num_layer)
 
 criterion = nn.CrossEntropyLoss()
 
-optimizer = torch.optim.Adam(net.parameters(),lr=0.05)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.05)
 
 for epoch in range(50):
-
     optimizer.zero_grad()
 
     target = net(inputs)
 
-    loss = criterion(target,labels)
+    loss = criterion(target, labels)
 
     loss.backward()
 
     optimizer.step()
-    #print(target.shape,target)
-    _,idx = target.max(dim=1)
+    # print(target.shape,target)
+    _, idx = target.max(dim=1)
 
-    print("Prodicted: ",''.join([idx_char[i] for i in idx]),end='')
-    print(', epoch[%d/10],loss:%.2f'%(epoch+1,loss.item()))
+    print("Prodicted: ", ''.join([idx_char[i] for i in idx]), end='')
+    print(', epoch[%d/10],loss:%.2f' % (epoch + 1, loss.item()))
--- practice/main.py
+++ practice/main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+# 01 背包问题
+def weightbag(weight, value, bagweight):
+    # 定义dp数组
+    dp = [[0 for _ in range(bagweight+1)] for _ in range(len(weight))]
+    print(dp)
+    # 数组初始化
+    for j in range(1,bagweight+1):
+        if weight[0] <= j:
+            dp[0][j] = value[0]
+    # 遍历顺序  先遍历物品在遍历背包
+    for i in range(1,len(weight)):
+        for j in range(1,bagweight+1):
+            # 如果当前物品重量大于背包重量 则不加改物品 当前价值就是最大价值
+            if weight[i] > j:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight[i]] + value[i])
+    return dp[-1][-1]
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    weight = [1, 3, 4,5]
+    value = [15, 20, 30,40]
+    bagweight = 5
+    print(weightbag(weight, value, bagweight))
+    # dp = [[0 for _ in range(4)]] * 3
+    # print(dp)
+    
